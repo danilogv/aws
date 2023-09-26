59c69cbf2049cf87cbef24a6c20733d725068104
danilogv <danilo.gv@hotmail.com>
2023-09-26 15:42:57 -0300

    Habilitando cors

--- back-end/src/main/java/com/danilo/aws/controle/FuncionarioControle.java
+++ back-end/src/main/java/com/danilo/aws/controle/FuncionarioControle.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/funcionario")
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 public class FuncionarioControle {
 
